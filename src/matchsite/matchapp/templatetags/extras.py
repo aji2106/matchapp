da9a098d82b8750613865965d820fd19c543a4b8
Anas Adan <anas__adan@hotmail.com>
2018-12-05 23:57:55 +0000

    extras.py

--- src/matchsite/matchapp/templatetags/extras.py
+++ src/matchsite/matchapp/templatetags/extras.py
@@ -5,23 +5,24 @@ register = template.Library()
 
 @register.filter
 def display_matches(matches):
+    html = ''
 
-    # for match in matches:
-    #    html += '<div class = "container" > \
-    #        <div class = "card-groups" >\
-    #        < div class = "card" >\
-    #        <img class = "card-img-top" src = "' + match.profile.image.url + ' " alt = "Card image cap" >\
-    #        < div class = "card-body" >\
-    #        < h5 class = "card-title" >' + match.username + ' < /h5 >\
-    #        < / div >\
-    #        < p class = "card-text" >\
-    #        < div > <b > Email: < /b > ' + match.profile.email + ' < /div >\
-    #        < div > <b > Age: < /b > '+match.profile.age + ' < /div >\
-    #        < div > <b > Gender: < /b > '+match.profile.gender + '< /div >\
-    #        < div > <b > Hobbies: < /b > ' + match.hobbies + ' < /div >\
-    #        < / p >\
-    #        < / div >\
-    #        < / div >\
-    #        < / div >'
+    for match in matches:
+            html += '<div class = "container" > \
+            <div class = "card-groups" >\
+            < div class = "card" >\
+            <img class = "card-img-top" src = "' + match.profile.image.url + ' " alt = "Card image cap" >\
+            < div class = "card-body" >\
+            < h5 class = "card-title" >' + match.username + ' < /h5 >\
+            < / div >\
+            < p class = "card-text" >\
+            < div > <b > Email: < /b > ' + match.profile.email + ' < /div >\
+            < div > <b > Age: < /b > '+match.profile.age + ' < /div >\
+            < div > <b > Gender: < /b > '+match.profile.gender + '< /div >\
+            < div > <b > Hobbies: < /b > ' + match.hobbies + ' < /div >\
+            < / p >\
+            < / div >\
+            < / div >\
+            < / div >'
 
-    return "hello this what I will append"
+    return html
