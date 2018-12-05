6fe4fc316bfa9d7db7c11fa1e89c2d55024227b3
aji2106 <ajiriowuasu@outlook.com>
2018-12-05 10:35:29 +0000

    Crispy forms

--- src/matchsite/matchsite/settings.py
+++ src/matchsite/matchsite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'crispy_forms',
     'matchapp',
 ]
 
@@ -68,6 +69,9 @@ TEMPLATES = [
         },
     },
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 
 WSGI_APPLICATION = 'matchsite.wsgi.application'
 
