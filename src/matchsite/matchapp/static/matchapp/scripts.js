8521391a132d974fcbb254af586f2529c0e549a2
Sergio X <sergio.arrieta.siyom@gmail.com>
2018-12-16 20:17:52 +0000

    Added modal
    Added terms and conditions modal to register page

--- src/matchsite/matchapp/static/matchapp/scripts.js
+++ src/matchsite/matchapp/static/matchapp/scripts.js
@@ -25,7 +25,41 @@ $(document).ready(function () {
    $("#id_password, #id_re_password").keyup(checkPasswordMatch);
 });
 ////////password ValidationError ends
+/////////terms and conditions modal
 
+$(document).ready(function () {
+  var modal = document.getElementById('myModal');
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+
+  // When the user clicks on the button, open the modal
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+});
+
+
+
+
+///////terms and conditions modal
 
 $(document).ready(function() {
     $( ".datepicker" ).datepicker({
