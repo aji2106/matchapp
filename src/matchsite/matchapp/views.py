6aad409d6b7b97a26bfe5a79175f7fb2cc6ef08f
Sergio X <sergio.arrieta.siyom@gmail.com>
2018-12-18 17:05:45 +0000

    Update views.py

--- src/matchsite/matchapp/views.py
+++ src/matchsite/matchapp/views.py
@@ -322,7 +322,11 @@ def editProfile(request, user):
             profile = Profile.objects.get(user=user.id)
             profile.email = form.cleaned_data.get('email')
 
-            if Profile.objects.filter(email = form.cleaned_data['email']).exists():
+                    # Get all the other users exclude current logged in user
+            exclude = Profile.objects.exclude(id=user.id)
+            
+
+            if exclude.filter(email = form.cleaned_data['email']).exists():
                 member = Member.objects.get(id=user.id)
                 allHobbies= member_form.cleaned_data.get('hobbies')
                 email=profile.email
