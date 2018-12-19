42ac27236494387d170ab6e0e05df6867c5ee6eb
Sergio X <sergio.arrieta.siyom@gmail.com>
2018-12-19 17:55:30 +0000

    merge
    merge

--- src/matchsite/matchapp/forms.py
+++ src/matchsite/matchapp/forms.py
@@ -52,19 +52,7 @@ class UserProfile(forms.ModelForm):
         model = Profile
         fields = ['email','dob','number','gender']
 
-        """def clean_email(self):
-        # Get the email
-        email = self.cleaned_data.get('email')
-
-        # Check to see if any users already exist with this email as a username.
-        try:
-            match = Profile.objects.get(email=email)
-        except Profile.DoesNotExist:
-            # Unable to find a user, this is fine
-            return email
-
-            # A user was found with this as a username, raise an error.
-            raise forms.ValidationError('This email address is already in use.')"""
+    
 
         widgets = {
             'email': forms.TextInput(attrs={
