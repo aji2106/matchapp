e32c2fd2ac884ccfb18f2d88b2f8d38952700988
Amair <a.naeem@se15.qmul.ac.uk>
2018-12-20 22:01:52 +0000

    removed files

--- matchsite/matchapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Profile)
-admin.site.register(Member)
-admin.site.register(Hobby)
-admin.site.register(Number)
-admin.site.register(Like)
